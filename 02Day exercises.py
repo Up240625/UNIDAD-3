5297b589d131ea70134e4fcee2c6028cc3303975
up240625 <up240625@alumnos.upa.edu.mx>
2025-02-26 15:13:10 -0600

    prueba 1

---
+++ 02Day exercises.py
@@ -0,0 +1,37 @@
+print('Hello, World')
+print(len('Hello, World'))
+print('Hello,World')
+print(len ('Hello,World'))
+       
+first_name = 'Fernando Jose '
+last_name= 'Flores Muñoz '
+country= 'Mexico '
+city= 'aguascalientes '
+age='18'
+is_married=False
+skills={'python'}
+person_info={
+    'firstname:Fernando Jose',
+     'lastname:Flores Muñoz', 
+     'country:Mexico',
+     'city:aguascalientes',
+}
+
+print('First_name', first_name)
+print('First_name length', len(first_name))
+print('Last_name',last_name)
+print('Last_name length',len(last_name))
+print('Country', country)
+print('City', city)
+print('Age', age)
+print('Married', is_married)
+print('Skills', skills)
+print('Person_info', person_info)
+
+print('first_name,last_name,country,age,is_married')
+print('First_name', first_name)
+print('Last_name',last_name)
+print('Country', country)
+print('Age', age)
+print('Married', is_married)
+
